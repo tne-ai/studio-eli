637f9d08895e51086247e971e1d1edf9b8e49cdc
eligotts <78387377+eligotts@users.noreply.github.com>
2024-11-17 11:11:55 -0600

    Create run_code.py

---
+++ Code/run_code.py
@@ -0,0 +1,11 @@
+from tne.TNE import TNE
+
+session = TNE(uid=UID, bucket_name=BUCKET, project=PROJECT, version=VERSION)
+
+
+if type(PROCESS_INPUT) is str:
+    text = PROCESS_INPUT 
+else:
+    text = "<ERROR>" 
+
+result = PROCESS_INPUT
