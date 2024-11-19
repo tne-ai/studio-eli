01c4bc696a32662c3ce712d4430b8013dbd3c39a
eligotts <78387377+eligotts@users.noreply.github.com>
2024-11-19 09:49:14 -0600

    Update test.py

--- Code/test.py
+++ Code/test.py
@@ -3,11 +3,15 @@ import csv
 
 session = TNE(uid=UID, bucket_name=BUCKET, project=PROJECT, version=VERSION)
 
-
-if type(PROCESS_INPUT) is str:
+if type(PROCESS_INPUT) is pd.DataFrame:
+    if PROCESS_INPUT.empty:
+        text = "<EMPTY DATAFRAME>"
+    else:
+        text = tabulate(PROCESS_INPUT, headers="keys", tablefmt="pipe", showindex=False)
+elif type(PROCESS_INPUT) is str:
     text = PROCESS_INPUT 
 else:
     text = "<ERROR>" 
 
-session.upload_object("test_output2.txt", text)
+session.upload_object("test_output4.txt", text)
 result = text
